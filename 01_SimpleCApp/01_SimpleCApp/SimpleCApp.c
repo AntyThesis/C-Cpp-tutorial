d8a3cbcfcb205fed2fa15ed9cd4692c96a595e0e
Ishmael Banks <Ibanks6602@yahoo.com>
2024-03-17 14:02:58 -0700

    Added functions

--- 01_SimpleCApp/01_SimpleCApp/SimpleCApp.c
+++ 01_SimpleCApp/01_SimpleCApp/SimpleCApp.c
@@ -7,14 +7,16 @@ void print_fx(int32_t x, int32_t y);
 
 int main()
 {
-
+	//(I) Simulate input (TODO: Replace with proper input)
 	int32_t x = 5;
 
+	// (P)
 	int32_t y = f(1 * x);
 	int32_t y2 = f(2 * x);
 	int32_t y3 = f(3 * x);
 	int32_t y4 = f(4 * x);
 
+	// (O)
 	print_fx(1 * x, y);
 	print_fx(2 * x, y2);
 	print_fx(3 * x, y3);
